8a935f7a0c0a9ec1734b90782b4e6ae8e8b2bc2b
troido <troido@protonmail.com>
2022-04-23 14:56:20 +0200

    tileset

--- scripts/Vehicle.gd
+++ scripts/Vehicle.gd
@@ -3,16 +3,16 @@ extends KinematicBody2D
 func _physics_process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed('ui_up'):
+	if Input.is_action_pressed('forward'):
 		velocity.y -= 200
 	
-	if Input.is_action_pressed('ui_down'):
+	if Input.is_action_pressed('backward'):
 		velocity.y += 200
 	
-	if Input.is_action_pressed('ui_left'):
+	if Input.is_action_pressed('turnleft'):
 		rotation_degrees -= 2
 	
-	if Input.is_action_pressed('ui_right'):
+	if Input.is_action_pressed('turnright'):
 		rotation_degrees += 2
 	
 	move_and_slide(velocity.rotated(rotation))
